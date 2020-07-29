4d7ea24c7dcd0c0299652f891d00f2fe9f3af552
SarahGrevy <65611758+SarahGrevy@users.noreply.github.com>
2020-07-28 20:32:41 -0400

    Update pages.py

--- Shared_reality/pages.py
+++ Shared_reality/pages.py
@@ -7,18 +7,15 @@ from .models import Constants
 class SRG(Page):
     form_model = 'player'
     form_fields = ['SRG_1', 'SRG_2', 'SRG_3', 'SRG_4', 'SRG_5', 'SRG_6', 'SRG_7', 'SRG_8']
+    
 class CO_CON(Page):
     form_model = 'player'
     form_fields = ['CO_CON_1', 'CO_CON_2', 'CO_CON_3', 'CO_CON_4', 'CO_CON_5']
+    
 class SRT(Page):
-    def is_displayed(self):
-        return self.round_number == 2
     form_model = 'player'
     form_fields = ['SRT_1', 'SRT_2', 'SRT_3', 'SRT_4', 'SRT_5']
-class IOS(Page):
-    form_model = 'player'
-    form_fields = ['IOS_1']
-
+    
 class IOS(Page):
     form_model = 'player'
     form_fields = ['IOS_1']
@@ -55,4 +52,4 @@ class open(Page):
     form_model = 'player'
     form_fields = ['open_1', 'open_2', 'reflect']
 
-page_sequence = [cert, sim, click, SRG, CO_CON, IOS, epist, relate, resp, rap, open,]
+page_sequence = [cert, sim, click, SRG, CO_CON, IOS, epist, relate, resp, rap, open]
