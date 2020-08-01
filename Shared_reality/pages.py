be413f2fb1511b25be54ecb64144512ca32627e2
SarahGrevy <65611758+SarahGrevy@users.noreply.github.com>
2020-08-01 16:37:36 -0400

    Update pages.py

--- Shared_reality/pages.py
+++ Shared_reality/pages.py
@@ -10,10 +10,6 @@ class SRG(Page):
     form_model = 'player'
     form_fields = ['SRG_1', 'SRG_2', 'SRG_3', 'SRG_4', 'SRG_5', 'SRG_6', 'SRG_7', 'SRG_8']
     
-class CO_CON(Page):
-    form_model = 'player'
-    form_fields = ['CO_CON_1', 'CO_CON_2', 'CO_CON_3', 'CO_CON_4', 'CO_CON_5']
-    
 class SRT(Page):
     form_model = 'player'
     form_fields = ['SRT_1', 'SRT_2']
@@ -33,10 +29,6 @@ class click(Page):
 class relate(Page):
     form_model = 'player'
     form_fields = ['rel_1', 'rel_2']
-
-class resp(Page):
-    form_model = 'player'
-    form_fields = ['resp_1', 'resp_2', 'resp_3']
 
 class sim(Page):
     form_model = 'player'
