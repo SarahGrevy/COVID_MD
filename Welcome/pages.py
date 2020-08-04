e9f7c3e5937229b2637473f5d4b838a471dba84f
SarahGrevy <65611758+SarahGrevy@users.noreply.github.com>
2020-08-04 16:04:39 -0400

    Update pages.py

--- Welcome/pages.py
+++ Welcome/pages.py
@@ -5,6 +5,15 @@ from .models import Constants
 
 class Consent(Page):
     form_model = 'player'
+class Global_1(Page):
+    form_model = 'player'
+    form_fields = ['pre_global_1']
+class Global_2(Page):
+    form_model = 'player'
+    form_fields = ['pre_global_2']
+class Global_3(Page):
+    form_model = 'player'
+    form_fields = ['pre_global_3']    
 class Zoom(Page):
     form_model = 'player'
 class Screen_setting(Page):
@@ -15,4 +24,6 @@ class MyWaitPage_1(WaitPage):
     pass
 class MyWaitPage_2(WaitPage):
     pass
-page_sequence = [Consent, MyWaitPage_1, Screen_setting, Icebreaker, MyWaitPage_2]
+class MyWaitPage_3(WaitPage):
+    pass
+page_sequence = [Consent, MyWaitPage_1, Global_1, Global_2, Global_3, MyWaitPage_2, Screen_setting, Icebreaker, MyWaitPage_2]
